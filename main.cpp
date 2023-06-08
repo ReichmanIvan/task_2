96bfc0d9476f3b336f9e6821cde8eb548a9427cd
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-06-08 13:21:32 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "..//list/List.h"
+
+int main()
+{
+	List first_list;
+	first_list.push_front(1);
+	first_list.push_front(2);
+	first_list.push_back(3);
+	first_list.push_front(4);
+	std::cout << first_list.get_size() << "\n";
+	std::cout << first_list.to_string() << "\t";
+	List second_list;
+	second_list = first_list;
+	std::cout << second_list.to_string();
+	return 0;
+};
